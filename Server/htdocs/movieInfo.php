b744265573182fad9a34fa9832981aeef2abea37
Anders Finn <anders@visiblethinking.com>
2012-10-26 13:49:50 -0700

    bad library

--- Server/htdocs/movieInfo.php
+++ Server/htdocs/movieInfo.php
@@ -38,6 +38,7 @@ foreach(json_decode($json, TRUE)  as $movie => $val){
 	// The jQuery autocomplete widget shows the label in the drop down,
 	// and adds the value property to the text box.
 
+        echo $movie;
 	$response[$i]['value'] = $movie->name;
 	$response[$i]['label'] = $movie->name . ' <small>(' . date('Y',strtotime($movie->released)).')</small>';
 	$i++;
