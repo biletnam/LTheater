65d6ce8277420e2331fc37e367f623a5912aebf9
Anders Finn <anders@visiblethinking.com>
2012-10-27 16:16:35 -0700

    Index

---
+++ Server/htdocs/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>LTheater Movie Control Panel</title>
+<link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+
+<body>
+<div id="page">
+    <div id="holder">
+        <a href=/add_movie.html>Upload a new movie</a>
+    </div>
+    
+</div>
+</body>
+</html>
